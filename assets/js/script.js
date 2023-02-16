9cfd5583a12b9933c0456a2814828f0afff45a7a
Chad Rajcooar <chadr208@gmail.com>
2023-02-16 12:21:31 -0500

    Modified Pseudocode to help delegate tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 // Dependencies
-    // 
-    // 
+    // Header (connected to time and date)
+    // Add project button
+    // Remove button
+    // Elements
 
 
 // DATA
@@ -11,18 +13,34 @@
 
 
 // Functions
+function startTimer() {
+    // Create date and time, MUST update every second (build with day.js)-- setInterval(), abbreviated month, day, full year, and time (e.g. Jun 30, 2022 at 08:37:48 am).
+
+}
+
+function displayProjects() {
+    // Create, Build, Place projects as table data
+}
     // Update clock every second (build with Day.JS) --setInterval(), abbreviated month, day, full year, and time (e.g. Jun 30, 2022 at 08:37:48 am).
-    //
+function loadSavedProjects() {
+    // get projects from local storage
+    // display them on the page
+}
+
+function addProjects() {
+    
+}
     //  
 
 
 
 // User Interactions
-    // Add project button
-    // Add X (delete) button
+    // Add: project button
+    // Add: X (delete) button
 
 
 // Initialization
-    // Create date and time, MUST update every second (build with day.js)-- setInterval(), abbreviated month, day, full year, and time (e.g. Jun 30, 2022 at 08:37:48 am).
-    // 
-    // 
+    // start timer
+    startTimer();
+    // load saved projects
+    loadSavedProjects();
