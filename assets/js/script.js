c697057bdf4987a31345693704258b08e52a51d3
Chad Rajcooar <chadr208@gmail.com>
2023-02-16 12:01:01 -0500

    Creating pseudocode

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+// Dependencies
+    // 
+    // 
+
+
+// DATA
+    // Create local storage
+    // * The name of the project.
+    //  The type of project (use a <select> drop-down).
+    // The date the project is due (use the date input type).
+
+
+// Functions
+    // Update clock every second (build with Day.JS) --setInterval(), abbreviated month, day, full year, and time (e.g. Jun 30, 2022 at 08:37:48 am).
+    //
+    //  
+
+
+
+// User Interactions
+    // Add project button
+    // Add X (delete) button
+
+
+// Initialization
+    // Create date and time, MUST update every second (build with day.js)-- setInterval(), abbreviated month, day, full year, and time (e.g. Jun 30, 2022 at 08:37:48 am).
+    // 
+    // 
